00f8d45eb0e8f778b4de11e70b2c6cb5e7ab8a38
Firetoad <fcalife@gmail.com>
2021-08-30 02:21:03 -0300

    Minor additional clean-up to collection

--- game/dota_addons/pathfinder/scripts/vscripts/common/battlepass/inventory/modifiers/modifier_cosmetic_skin_living_energy.lua
+++ game/dota_addons/pathfinder/scripts/vscripts/common/battlepass/inventory/modifiers/modifier_cosmetic_skin_living_energy.lua
@@ -8,3 +8,11 @@ function modifier_cosmetic_skin_living_energy:GetAttributes() return MODIFIER_AT
 function modifier_cosmetic_skin_living_energy:GetStatusEffectName()
 	return "particles/collection/hero_skins/living_energy_status_fx.vpcf"
 end
+
+function modifier_cosmetic_skin_living_energy:GetEffectName()
+	return "particles/collection/hero_skins/living_energy.vpcf"
+end
+
+function modifier_cosmetic_skin_living_energy:GetEffectAttachType()
+	return PATTACH_ABSORIGIN_FOLLOW
+end
